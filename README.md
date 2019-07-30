ec907add0a00c516e17d343621b76b3852d07713
Gabby Gurdin <ggurdin@gmail.com>
2019-07-30 14:37:47 -0400

    Initial repo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Medalogies
+
+Medical Word Embeddings
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+import setuptools
+
+setup(name='medalogies',
+      version='0.0.1',
+      description='Medical Word Embeddings',
+      author='Gabby Gurdin',
+      license='MIT',
+      packages=setuptools.find_packages())
